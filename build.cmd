be78ba6ac29f0ede65c0fe9f652c3c9b7deb3bb7
Giovanni Bassi <giggio@giggio.net>
2013-12-04 10:31:51 -0200

    gruntfile on solution root, not proj root anymore. build and publish scripts

---
+++ build.cmd
@@ -0,0 +1,2 @@
+del *.nupkg
+nuget pack "%~dp0Grunt.js.nuspec" %*
--- tools/install.ps1
+++ tools/install.ps1
@@ -1,5 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
+cp $toolsPath\Gruntfile.coffee $installPath\..\..\Gruntfile.coffee
 $npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15.2\tools\npm.cmd")
 & $npmPath install grunt-cli
 & $npmPath install grunt
