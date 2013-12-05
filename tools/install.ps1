d7a35db0ff41b357fe35cb7084ae0889cdf674b7
Giovanni Bassi <giggio@giggio.net>
2013-12-05 13:41:12 -0200

    updating npm, and making it use the latest version automatically, and save on npm install

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 cp $toolsPath\Gruntfile.coffee $installPath\..\..\Gruntfile.coffee
-$npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15.3\tools\npm.cmd")
-& $npmPath install grunt-cli
-& $npmPath install grunt
+$npmPath = (join-path $toolsPath "npm.cmd")
+& $npmPath install grunt-cli --save-dev
+& $npmPath install grunt --save-dev
