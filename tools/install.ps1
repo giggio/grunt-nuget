8ad6a3fcb92ad3c76c000524d9b709b0d9bc2a9b
Giovanni Bassi <giggio@giggio.net>
2013-12-04 22:13:52 -0200

    update npm

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 cp $toolsPath\Gruntfile.coffee $installPath\..\..\Gruntfile.coffee
-$npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15.2\tools\npm.cmd")
+$npmPath = (join-path (join-path $installPath "..") "Npm.js.1.3.15.3\tools\npm.cmd")
 & $npmPath install grunt-cli
 & $npmPath install grunt
